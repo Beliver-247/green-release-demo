ccec8a0cdedb0fccfc71cfb5565b7c9fcf463721
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2026-07-03 13:14:59 +0530

    style: remove unnecessary test comment from application entry point

--- app/src/main/java/com/greenrelease/app/GreenReleaseDemoApplication.java
+++ app/src/main/java/com/greenrelease/app/GreenReleaseDemoApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.ComponentScan;
  * Spring Boot application entry point
  * Configures all modules and wires dependencies
  */
-//comment for test
 @SpringBootApplication
 @ComponentScan(basePackages = {
         "com.greenrelease.api.controller",
