4ef1eb90fcbcfa360a2ebdca593c69d4e56eb75f
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2026-08-23 17:53:46 +0530

    commented code[bypass-green]

--- api/src/main/java/com/greenrelease/api/controller/HealthController.java
+++ api/src/main/java/com/greenrelease/api/controller/HealthController.java
@@ -16,7 +16,7 @@ public class HealthController {
     private final HealthService healthService;
 
     public HealthController(HealthService healthService) {
-        System.out.println("hi");
+        //System.out.println("hi");
         this.healthService = healthService;
     }
 
