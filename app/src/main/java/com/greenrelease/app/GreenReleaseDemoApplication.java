89f28ac41d4be93a8e6d90773152a2b557809d44
Dumindu Mendis <121997251+Beliver-247@users.noreply.github.com>
2026-08-23 17:18:09 +0530

    feat: add initialization log to healthService bean

--- app/src/main/java/com/greenrelease/app/GreenReleaseDemoApplication.java
+++ app/src/main/java/com/greenrelease/app/GreenReleaseDemoApplication.java
@@ -28,6 +28,7 @@ public class GreenReleaseDemoApplication {
      */
     @Bean
     public HealthService healthService() {
+        System.out.println("healthservice init");
         return new HealthService();
     } 
 
